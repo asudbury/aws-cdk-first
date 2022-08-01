7f791975288d2c4cc89fb50ca06551c20eb302e7
Adrian Sudbury <sudburya@gmail.com>
2022-08-01 12:02:00 +0100

    Initial commit

---
+++ test/aws-cdk-first.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkFirst from '../lib/aws-cdk-first-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-first-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkFirst.AwsCdkFirstStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
