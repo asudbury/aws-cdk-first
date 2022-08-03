efd9d3a0b2e22079adf804081d5a3883c335e927
Adrian Sudbury <sudburya@gmail.com>
2022-08-03 09:50:38 +0100

    Initial Version

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
-# Welcome to your CDK TypeScript project
+# Welcome to my first AWS CDK project
 
-This is a blank project for CDK development with TypeScript.
+This is my first project for CDK development with TypeScript.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+- `cdk destroy` destroys the stack
+
+## Useful references
+
+- https://docs.aws.amazon.com/cdk/api/v1/docs/aws-construct-library.html
+- https://docs.aws.amazon.com/cdk/v2/guide/home.html
+- https://cdkworkshop.com/20-typescript.html
+
+## Implementation
+
+The following has been implemented
+
+- VPC - https://docs.aws.amazon.com/cdk/api/v1/docs/@aws-cdk_aws-ec2.Vpc.html
+- S3 Bucket - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-s3-readme.html
+- Lambda Function - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-lambda-readme.html
+- Api Gateway - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-apigateway-readme.html
+- SQS Queue - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-sqs-readme.html
+- SNS Topic - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-sns-readme.html
+- SNS Subscription - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-sns-subscriptions-readme.html
+- DynamoDB - https://docs.aws.amazon.com/cdk/api/v1/docs/aws-dynamodb-readme.html
--- lib/aws-cdk-first-stack.ts
+++ lib/aws-cdk-first-stack.ts
@@ -1,16 +1,78 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class AwsCdkFirstStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /// VPC
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkFirstQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ec2.Vpc(this, 'awsCdkFirstVPC', {
+      vpcName: 'aws-cdk-first-vpc',
+      cidr: '10.0.0.0/16',
+    });
+
+    /// s3 Bucket
+
+    const bucket = new s3.Bucket(this, 'awsCdkFirstS3Bucket', {
+      bucketName: 'aws-cdk-first-s3bucket',
+    });
+
+    /// Lambda Function
+
+    const lambdaFunction = new lambda.Function(this, 'HelloHandler', {
+      functionName: 'HelloHandler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+      environment: {
+        BUCKET: bucket.bucketName,
+      },
+    });
+
+    bucket.grantReadWrite(lambdaFunction);
+
+    /// Api Gateway
+
+    const api = new apigateway.RestApi(this, 'awsCdkFirstApiGateway', {
+      restApiName: 'aws-cdk-first-api-gateway',
+    });
+
+    const lambdaIntegration = new apigateway.LambdaIntegration(lambdaFunction, {
+      requestTemplates: { 'application/json': '{ "statusCode": "200" }' },
+    });
+
+    api.root.addMethod('GET', lambdaIntegration);
+
+    /// SQS Queue
+
+    const queue = new sqs.Queue(this, 'awsCdkFirstQueue', {
+      queueName: 'aws-cdk-first-queue',
+      retentionPeriod: Duration.days(7),
+      visibilityTimeout: Duration.seconds(300),
+    });
+
+    /// SNS Topic
+
+    const topic = new sns.Topic(this, 'awsCdkFirstTopic');
+
+    /// SNS Subscription
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    /// DynamoDB
+
+    new dynamodb.Table(this, 'awsCdkFirstDynamoDb', {
+      tableName: 'aws-cdk-first-dynamodb',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.NUMBER },
+    });
   }
 }
